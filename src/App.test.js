71fe61c8423b3e5464a854e6b03ca744e0f89316
Divyanshu Maithani <div.blackcat@gmail.com>
2017-04-16 16:24:57 +0530

    Add some tests with Jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import renderer from 'react-test-renderer';
+// import App from './App';
+import ReactAnimatedWeather from './ReactAnimatedWeather';
 
-it('renders without crashing', () => {
+it('exports ReactAnimatedWeather component', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<ReactAnimatedWeather />, div);
+  // Jest can't figure out <canvas> elements
 });
+
+// test('ReactAnimatedWeather is rendered properly', () => {
+//   const component = renderer.create(
+//     <ReactAnimatedWeather />
+//   );
+//
+//   const tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+import ReactAnimatedWeather from './ReactAnimatedWeather';
+
+export default ReactAnimatedWeather;
