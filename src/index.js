ac1db0becc64bd0f6819c6f3724c8db3e76ebdc3
Divyanshu Maithani <div.blackcat@gmail.com>
2017-04-17 11:07:56 +0530

    More fixes v0.1.3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import './index.css';
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
 import ReactAnimatedWeather from './ReactAnimatedWeather';
 
 export default ReactAnimatedWeather;
